10670ded76ef2fcbfb7c9802a7b0a986438851a0
somsomi3 <chfhddlfhqls@naver.com>
2024-12-14 00:53:15 +0900

    add Spring Boot

---
+++ StarlightWing/src/main/java/com/minjoo/StarlightWing/StarlightWingApplication.java
@@ -0,0 +1,13 @@
+package com.minjoo.StarlightWing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlightWingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlightWingApplication.class, args);
+	}
+
+}
